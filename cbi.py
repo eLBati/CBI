a7f71277a4c2c3250ecf960cba3fd8fab3c1da8c
eLBati <lorenzo.battistini@agilebg.com>
2011-05-16 17:39:51 +0200

    [IMP] IMRecord & EFRecord

--- cbi.py
+++ cbi.py
@@ -23,21 +23,20 @@ import time
 
 class Field():
 
-    def __init__(self, fromposition, toposition, name, required=False, tp='an', check='N', desc=''):
+    def length(self):
+        return (self.toposition - self.fromposition) + 1
+
+    def __init__(self, fromposition, toposition, name, content=''):
         self.fromposition = fromposition
         self.toposition = toposition
         self.name = name
-        self.required = required
-        self.type = tp
-        self.check = check
-        self.desc = desc
-        self.content = ''
+        if not content:
+            self.content = ''.ljust(self.length())
+        else:
+            self.content = content
 
     def __str__(self):
         return self.content
-
-    def __getitem__(self, key):
-        return self.content(key)
 
 class Record():
 
@@ -45,7 +44,90 @@ class Record():
         self.code = code
         self.fields = []
 
+    def __str__(self):
+        c = ''
+        for field in self.fields:
+            c = c + field.content
+        return c
+
+    def __getitem__(self, key):
+        """Overloading in order to retrieve content
+            by position or field name specified in CBI docs"""
+        if isinstance(key, slice) and not key.step:
+            return self[key.start-1:key.stop]
+        elif isinstance(key, str):
+            for field in self.fields:
+                if field.name == key:
+                    return field.content
+            raise IndexError('Impossible to find field with that key')            
+        else:
+            return self[key]
+
+    def __setitem__(self, key, item):
+        """Overloading in order to write content
+            by position or field name specified in CBI docs"""
+        if isinstance(key, slice) and not key.step:
+            for field in self.fields:
+                if field.fromposition == key.start and field.toposition == key.stop:
+                    field.content = item
+                    return
+            raise IndexError('Impossible to find field with that position')
+        elif isinstance(key, str):
+            for field in self.fields:
+                if field.name == key:
+                    if len(item) > field.length():
+                        raise BufferError('Specified field value passes field capacity')
+                    else:
+                        field.content = item.ljust(field.length())
+                        return
+            raise IndexError('Impossible to find field with that key') 
+        else:
+            raise TypeError('You must use slice or string to access fields list')
+
+    def appendfield(self, field):
+        if not isinstance(key, Field):
+            raise TypeError('You can only append Field objects')
+        if field.name in [n for f.n in self.fields]:
+            raise IndexError('Field name already present')
+        self.fields.append(field)
+
 class IMRecord(Record):
 
     def __init__(self):
-        Record.__init__('IM')
+        Record.__init__(self, 'IM')
+        self.appendfield(Field(1, 1, 'filler1'))
+        self.appendfield(Field(2, 3, 'tipo_record', content='IM'))
+        self.appendfield(Field(4, 8, 'mittente')
+        self.appendfield(Field(9, 13, 'ricevente'))
+        self.appendfield(Field(14, 19, 'data_creazione'))
+        self.appendfield(Field(20, 39, 'nome_supporto'))
+        self.appendfield(Field(40, 45, 'campo_a_disposizione'))
+        self.appendfield(Field(46, 104, 'filler2'))
+        self.appendfield(Field(105, 105, 'tipo_flusso'))
+        self.appendfield(Field(106, 106, 'qualificatore_flusso'))
+        self.appendfield(Field(107, 111, 'soggetto_veicolatore'))
+        self.appendfield(Field(112, 113, 'filler3'))
+        self.appendfield(Field(114, 114, 'codice_divisa'))
+        self.appendfield(Field(115, 115, 'filler4'))
+        self.appendfield(Field(116, 120, 'campo_non_disponibile'))
+
+class EFRecord(Record):
+
+    def __init__(self):
+        Record.__init__(self, 'IM')
+        self.appendfield(Field(1, 1, 'filler1'))
+        self.appendfield(Field(2, 3, 'tipo_record', content='EF'))
+        self.appendfield(Field(4, 8, 'mittente')
+        self.appendfield(Field(9, 13, 'ricevente'))
+        self.appendfield(Field(14, 19, 'data_creazione'))
+        self.appendfield(Field(20, 39, 'nome_supporto'))
+        self.appendfield(Field(40, 45, 'campo_a_disposizione'))
+        self.appendfield(Field(46, 52, 'numero_disposizioni'))
+        self.appendfield(Field(53, 67, 'tot_importi_negativi'))
+        self.appendfield(Field(68, 82, 'tot_importi_positivi'))
+        self.appendfield(Field(83, 89, 'numero_record'))
+        self.appendfield(Field(90, 113, 'filler2'))
+        self.appendfield(Field(114, 114, 'codice_divisa'))
+        self.appendfield(Field(115, 120, 'campo_non_disponibile'))
+
+
